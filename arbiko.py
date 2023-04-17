7cc03c2f60661bafadc8f56b434f08c692a348b2
Tomasz987 <tomek987125@gmail.com>
2023-04-17 20:21:25 +0200

    Create the docstrings and add typing in the arbiko.py module

--- arbiko.py
+++ arbiko.py
@@ -1,3 +1,4 @@
+"""The module to scrape http://arbiko.pl site."""
 from datetime import datetime
 from requests import Session
 
@@ -7,7 +8,23 @@ from exceptions import LoginError
 
 
 class Arbiko:
-    def __init__(self, username, password, user_agent):
+    """The collections of the tools to scrape arbiko.pl site.
+    The class has implemented the necessary methods to use as a context manager.
+
+    Methods:
+        login():
+        get_order_history(start_date: str, end_date: str): fetches and return the order history
+            for the passed time period
+        get_oem_number(catalog_number: str): fetches oem number for the passed catalog number
+    """
+    def __init__(self, username: str, password: str, user_agent: str):
+        """Construct all the necessary attributes for the arbiko object.
+
+        Args:
+            username (str): username to login in aribko.pl site
+            password (str): password to login in aribko.pl site
+            user_agent (str): user agent
+        """
         self.history_url = 'http://arbiko.pl/arbos/search_zam.php3?ref=zamowienia'
         self.search_url = 'http://arbiko.pl/arbos/search_of.php3?ref=oferta'
         self.login_url = 'http://arbiko.pl/arbos/loguj1.php3'
@@ -26,12 +43,18 @@ class Arbiko:
         pass
 
     def _set_headers(self):
+        """Set user agent in headers."""
         self.headers = {
             'User-Agent': self.user_agent
         }
         self.session.headers = self.headers
 
-    def login(self):
+    def login(self) -> bool:
+        """The method try to login at aribko.pl.
+
+        Returns:
+            True(bool): if login was correct
+            False(bool): if login was incorrectly."""
         login_payload = {
             'user': self.username,
             'passwd': self.password,
@@ -47,7 +70,17 @@ class Arbiko:
             else:
                 return False
 
-    def get_order_history(self, start_date, end_date):
+    def get_order_history(self, start_date: str, end_date: str) -> dict:
+        """The method fetches and return the order history
+            for the passed time period.
+
+        Args:
+            start_date (str): start date to get order history
+            end_date (str): end date to get order history
+
+        Returns:
+            result(dict): fetched data
+        """
         history_payload = {
             'filters': 'data_od,data_do,numer,stan',
             'data_od': start_date,
@@ -91,8 +124,8 @@ class Arbiko:
                 details = value.find_all('td')[1:]
                 if len(details) > 4:
                     cat_num, desc, _, quantity, *_ = details
+                    # The catalog number can't start at zero
                     cat_num_without_zero = cat_num.text[1:] if cat_num.text[0] == '0' else cat_num.text
-                    print(cat_num_without_zero)
                     product = {
                         'catalog_number': cat_num.text,
                         'oem_number': self.get_oem_number(cat_num_without_zero),
@@ -103,7 +136,15 @@ class Arbiko:
 
         return result
 
-    def get_oem_number(self, catalog_number):
+    def get_oem_number(self, catalog_number: str) -> str:
+        """The method fetches and return oem number for the passed catalog number.
+
+        Args:
+            catalog_number (str): product catalog number to get oem number
+
+        Returns:
+            (str): oem number or string "???? ????" if was error
+        """
         search_payload = {
             'keyw': catalog_number,
         }
