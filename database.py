e4614cda21df3a2d12eb9dd5c306bb5aea6e97aa
Tomasz987 <tomek987125@gmail.com>
2023-04-17 20:57:55 +0200

    Create the docstrings in the database.py module

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+"""The collections of tools to manage the database."""
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
 
@@ -6,7 +7,16 @@ from protection import Protection
 
 
 class Database:
-    def __init__(self, database_path):
+    """The collections of the tools to manage the database.
+    The class has implemented the necessary methods to use as a context manager.
+
+    Methods:
+         create_database(): create the database if not exists
+         dump(): dump the data from the database and return as bytes
+         load(): load the data from the protected file and load it to database
+    """
+    def __init__(self, database_path: object):
+        """Construct all the necessary attributes for the database object."""
         self.database_path = database_path
         self.engine = create_engine(f'sqlite:///:memory:', future=True)
         self.session = None
@@ -19,16 +29,19 @@ class Database:
         Protection(self.database_path).save_database_dump(self.dump())
 
     def create_database(self):
+        """Create the database if not exists."""
         if self.database_path.exists():
             raise FileExistsError
 
         Base.metadata.create_all(self.engine)
 
     def create_session(self):
+        """Create database session."""
         with Session(self.engine) as session:
             self.session = session
 
     def dump(self) -> bytes:
+        """Dump the data from the database and return as bytes."""
         connection = self.engine.raw_connection()
         result = b''
 
@@ -38,6 +51,7 @@ class Database:
         return result
 
     def load(self):
+        """Load the data from the protected file and load it to database."""
         content = Protection(self.database_path).decrypt_file()
         scripts = content.split(';')
         for script in scripts:
