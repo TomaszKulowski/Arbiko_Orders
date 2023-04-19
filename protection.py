c71b93b2c7e1723b5905f60397e1de6a6a363c05
Tomasz987 <tomek987125@gmail.com>
2023-04-19 20:53:42 +0200

    Create the docstrings in the protection.py file

--- protection.py
+++ protection.py
@@ -1,3 +1,4 @@
+"""The collections of the tools to encrypt and decrypt the database file."""
 import base64
 import gzip
 
@@ -9,11 +10,31 @@ from cryptography.hazmat.backends import default_backend
 
 
 class Protection:
-    def __init__(self, password, name):
+    """The class to encrypt and decrypt database.
+
+    Methods:
+        key_creation(): create the key for the encryption
+        encrypt(data: str): encrypt passed data and return it
+        decrypt(data: str): decrypt passed data and return it
+        decrypt_file(): decrypt database file and return content
+        save_database_dump(database_dump: str): encrypt database dump and save to the file
+    """
+    def __init__(self, password: str, name: str):
+        """Construct all the necessary attributes for the protection object.
+
+        Args:
+            password (str): database password
+            name (str): database name
+        """
         self.password = bytes(password)
         self.name = name
 
     def key_creation(self) -> cryptography.fernet.Fernet:
+        """Create the key for the encryption.
+
+        Returns:
+            object (cryptographt.fernet.Fernet): key for encrypt and decrypt data
+        """
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             salt=b'\xfaz\xb5\xf2|\xa1z\xa9\xfe\xd1F@1\xaa\x8a\xc2',
@@ -25,17 +46,38 @@ class Protection:
         key = Fernet(base64.urlsafe_b64encode(kdf.derive(self.password)))
         return key
 
-    def encrypt(self, data):
+    def encrypt(self, data: str) -> str:
+        """Encrypt and return passed data.
+
+        Args:
+            data (str): data to encrypt
+
+        Returns:
+            (str): encrypted data
+        """
         key = self.key_creation()
         safe = key.encrypt(data)
         return safe
 
-    def decrypt(self, data):
+    def decrypt(self, data: str) -> str:
+        """Decrypt and return passed data.
+
+        Args:
+            data (str): data to decrypt
+
+        Returns:
+            (str): decrypted data
+        """
         key = self.key_creation()
         result = key.decrypt(data)
         return result
 
     def decrypt_file(self) -> str:
+        """Open, decrypt and return database content.
+
+        Returns:
+            (str): database content
+        """
         file = gzip.open(self.name.name, 'rb')
         data = file.read()
         file.close()
@@ -45,7 +87,8 @@ class Protection:
 
         return content
 
-    def save_database_dump(self, database_dump):
+    def save_database_dump(self, database_dump: str):
+        """Encrypt and save to the file passed database dump."""
         encrypted_data = self.encrypt(database_dump)
 
         file = gzip.open(self.name, 'wb')
